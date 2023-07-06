73cbd6c7e0187e0020f03ddcb95db350c4a9b835
César <cesarsantossouza99@gmail.com>
2023-07-06 14:55:51 -0300

    js

---
+++ script.js
@@ -0,0 +1,46 @@
+const resul = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+const playHuman = (humanChoice) => {
+
+    playTheGame(humanChoice, playMachine())
+
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    console.log('Humano: ' + human + ' Maquina: ' + machine);
+
+    if (human === machine) {
+        resul.innerHTML = "Deu empate"
+    } else if (
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        resul.innerHTML = "Você ganhou"
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        resul.innerHTML = "Você perdeu para Alexa"
+    }
+}
+
+
+
+
+
+
+
